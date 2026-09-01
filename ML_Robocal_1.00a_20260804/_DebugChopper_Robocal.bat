39e21e09a107260cb05eae7589c0c37569e3e16b
Flea Wu (PEGATRON CORPORATION) <v-fleawu@microsoft.com>
2026-09-01 17:31:08 +0800

    Update RoboCal diagnostic workflow

--- ML_Robocal_1.00a_20260804/_DebugChopper_Robocal.bat
+++ ML_Robocal_1.00a_20260804/_DebugChopper_Robocal.bat
@@ -1,6 +1,6 @@
 @ECHO ON
 del *.log
-del *.dat
+
 DEL *.TXT
 CD %~dp0..
 COPY DiagPGM\OP.dat OP.dat 
@@ -10,10 +10,10 @@ CALL CONFIG.BAT SmokeTest offline
 move *.dat %~dp0
 CD %~dp0
 
-IF NOT EXIST SN.dat ECHO THISISATESTSNUMB>SN.dat
 IF NOT EXIST OP.dat ECHO S12345678>OP.dat
 
 SET CFG_NAME=config_Deb.xml
+
 Chopper-diag.exe -c -CGV -opf op.dat -SNF SN.dat -sip -TSRID -lock -RL -f %CFG_NAME% -e -rs -c2e -adr -SNP "^[0-9,A-Z]{12}$" /r
 IF %ERRORLEVEL% EQU 250 pause & exit /b
 IF %ERRORLEVEL% EQU 251 pause & exit /b
